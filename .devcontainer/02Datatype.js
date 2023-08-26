efd64eac928943dc3fa18addee8b757bd6ef8279
Rashmina123 <97404736+Rashmina123@users.noreply.github.com>
2023-08-26 19:10:50 +0530

    Add files via upload

---
+++ .devcontainer/02Datatype.js
@@ -0,0 +1,18 @@
+'use strictmode' //treats code as a newer version
+
+//tc39. EcmaScript Language Grammer
+
+//alert('Hello') // we r using node js not BROWSER.
+/* DataTypes Premitive Datatypes
+string 
+number
+boolean
+NUll -- representation of Empty. 
+Undefine == means not assign any value.
+Symbol == for uniq
+*/
+
+let a = null
+console.log(a)
+console.log(typeof(null)) // it gives object
+console.log(typeof(undefine)) // it gives undefined
